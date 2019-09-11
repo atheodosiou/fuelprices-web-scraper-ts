b5e5640a2ea66fc1a32b25eecbfe8dda1a476b0a
Anastasios Theodosiou <anastasios.theodosiou@gmail.com>
2019-09-11 22:29:03 +0300

    Set up nodemon

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,43 @@
-import * as data from '../data/connections.json';
-import { Connection, Connections } from './models/connection.model.js';
-import { DdLink } from './models/ddLink.model.js';
-import { MunicipalDistrictLink } from './scripts/generateDdLinks.js';
-import { SaveLinksToFile } from './scripts/saveLinksToFile.js';
-import { performance } from 'perf_hooks';
+import * as data from "../data/connections.json";
+import { Connections } from "./models/connection.model";
+import { DdLink } from "./models/ddLink.model";
+import { MunicipalDistrictLink } from "./scripts/generateDdLinks";
+import { SaveLinksToFile } from "./scripts/saveLinksToFile";
+import { performance } from "perf_hooks";
 
 export class App {
-    async bootstrapApp() {
-        const startDt= performance.now();
-        
-        console.log('App started...\n');
-        await this.createLinksFile();
+  async bootstrapApp() {
+    const startDt = performance.now();
 
-        const endDt = performance.now();
-        const duration = endDt-startDt;
-        console.log(`Done! Total time: ${duration}ms.\nProcess exit.`);
-        await process.exit(0);
-    }
+    console.log("App started...\n");
+    await this.createLinksFile();
 
-    private async createLinksFile(){
-        
+    const endDt = performance.now();
+    const duration = endDt - startDt;
+    console.log(`Done! Total time: ${duration}ms.\nProcess exit.`);
+    await process.exit(0);
+  }
 
-        let relationships: Connections;
-        relationships = JSON.parse(JSON.stringify(data));
-        const connections = relationships.connections;
+  private async createLinksFile() {
+    let relationships: Connections;
+    relationships = JSON.parse(JSON.stringify(data));
+    const connections = relationships.connections;
 
-        let links: DdLink[];
-        //Generate municipal district links
-        const municipalDistrictLink = new MunicipalDistrictLink();
+    let links: DdLink[];
+    //Generate municipal district links
+    const municipalDistrictLink = new MunicipalDistrictLink();
 
-        links = municipalDistrictLink.generateLinks(connections);
-        console.log('Total links generated: ', links.length);
+    links = municipalDistrictLink.generateLinks(connections);
+    console.log("Total links generated: ", links.length);
 
-        const saveLinksToFile = new SaveLinksToFile();
-        await saveLinksToFile.writeJSON(links, 'data/', 'links.json').then(() => {
-            console.log('File was created successfully!\n')
-        }).catch(error => {
-            console.log('File was not created successfully!\n\n', error);
-        });
-    }
+    const saveLinksToFile = new SaveLinksToFile();
+    await saveLinksToFile
+      .writeJSON(links, "data/", "links.json")
+      .then(() => {
+        console.log("File was created successfully!\n");
+      })
+      .catch(error => {
+        console.log("File was not created successfully!\n\n", error);
+      });
+  }
 }
